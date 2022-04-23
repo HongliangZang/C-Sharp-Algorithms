9090fcbb8c1c9f7cc02221b2467ac9625972c010
hongliang.zang <22933133+HongliangZang@users.noreply.github.com>
2022-04-23 08:38:27 +0100

    FibonacciNumber

---
+++ Algorithms/Numeric/FibonacciNumber.cs
@@ -0,0 +1,18 @@
+﻿namespace Algorithms.Numeric
+{
+    public static class FibonacciNumber
+    {
+        public static int GetFibonacciNumber(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    return 0;
+                case 1:
+                    return 1;
+                default:
+                    return GetFibonacciNumber(index - 1) + GetFibonacciNumber(index - 2);
+            }
+        }
+    }
+}
---
+++ UnitTest/AlgorithmsTests/FibonacciNumberTests.cs
@@ -0,0 +1,37 @@
+﻿using Algorithms.Numeric;
+using Xunit;
+
+namespace UnitTest.AlgorithmsTests
+{
+    public class FibonacciNumberTests
+    {
+        [Fact]
+        public void Should_return_zero()
+        {
+            var result = FibonacciNumber.GetFibonacciNumber(0);
+            Assert.True(result.Equals(0));
+        }
+        
+        [Fact]
+        public void Should_return_one()
+        {
+            var result = FibonacciNumber.GetFibonacciNumber(1);
+            Assert.True(result.Equals(1));
+        }
+        
+        [Fact]
+        public void Should_return_one_for_position_two()
+        {
+            var result = FibonacciNumber.GetFibonacciNumber(2);
+            Assert.True(result.Equals(1));
+        }
+        
+        
+        [Fact]
+        public void Should_return_correct_value_for_position_eight()
+        {
+            var result = FibonacciNumber.GetFibonacciNumber(8);
+            Assert.True(result.Equals(21));
+        }
+    }
+}
